562342faae19f823b64ce5ae432f2448c62ce99f
Mary Kuzenko <Mary Kuzenko>
2017-01-31 21:34:47 +0200

    Дз№1

---
+++ src/SecondClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Admin on 31.01.2017.
+ */
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("My first program on Java...");
+
+    }
+}
